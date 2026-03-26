00010d2232a686a49c0e363981b5571a2b661652
David Galera <dgalrod@proton.me>
2026-03-26 19:18:14 +0000

    chore(next): allow remote images from product CDN
    Configure images.remotePatterns for dulces-petalos.jakala.es so Next/Image can load product photos.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dulces-petalos.jakala.es",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
